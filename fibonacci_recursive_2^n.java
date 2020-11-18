25eda2235a1e552f12461cc974252af6de7d5121
KashifNiaz59 <65736149+KashifNiaz59@users.noreply.github.com>
2020-11-18 00:44:02 -0800

    Create fibonacci_recursive_2^n.java

---
+++ fibonacci_recursive_2^n.java
@@ -0,0 +1,33 @@
+// Fibonacci Series  --- RECURSIVE METHOD ---
+// Time Complexity : O(2^n)
+// input : integer --> how many fibonnaci number be represented --> 5
+// output : show fibonacci series --> 0,1,1,2,3,5,8,13,...
+// handles : nothing
+
+public class Fibonacci_Recursive_2 {
+
+    // calculate the fibonacci number
+    // input : fibonacci series number
+    // output : return fibonacci series number
+    public int fib(int aNum){
+        if(aNum==0)
+            return 0;
+        else if(aNum==1)
+            return 1;
+        else
+            return(fib(aNum-1)+fib(aNum-2));
+    }
+
+
+    public static void main(String args[]){
+        Fibonacci_Recursive_2 fib=new Fibonacci_Recursive_2();
+
+        // enter number how many fibonacci series return
+        int num=8;
+
+        for(int i=0;i<num;i++){
+            System.out.print(fib.fib(i)+" ");
+        }
+
+    }// --> main
+}// --> class
